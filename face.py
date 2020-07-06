14be7d2b84b0b9ca7d30c3c66aa273d28e90f21f
Manan Shah <manan.vinod.shah@gmail.com>
2020-07-06 20:13:33 +0530

    Update face.py

--- face.py
+++ face.py
@@ -4,15 +4,9 @@
 import cv2  
   
 # load the required trained XML classifiers 
-# https://github.com/Itseez/opencv/blob/master/ 
-# data/haarcascades/haarcascade_frontalface_default.xml 
 # Trained XML classifiers describes some features of some 
-# object we want to detect a cascade function is trained 
-# from a lot of positive(faces) and negative(non-faces) 
-# images. 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
   
-# https://github.com/Itseez/opencv/blob/master 
 # /data/haarcascades/haarcascade_eye.xml 
 # Trained XML file for detecting eyes 
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')  
@@ -21,12 +15,11 @@ eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 cap = cv2.VideoCapture(0) 
   
 # loop runs if capturing has been initialized. 
-while 1:  
+while True:  
   
-    # reads frames from a camera 
     ret, img = cap.read()  
     img = cv2.flip(img, 1)
-    # convert to gray scale of each frames 
+    # convert to gray scale 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
   
     # Detects faces of different sizes in the input image 
